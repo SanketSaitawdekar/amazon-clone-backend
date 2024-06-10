ae4dd873d70137342eecf5cba2ffbdb08db7ae5e
SanketSaitawdekar <sanketsaitawdekar@github.com>
2024-06-10 20:01:28 +0530

    first commit

---
+++ src/test/java/com/sanket/amazonclone/AmazoncloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanket.amazonclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazoncloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
